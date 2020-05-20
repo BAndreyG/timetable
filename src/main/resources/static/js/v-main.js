b93c249053bbe2a677115ae7c1e6e1f9d40b366a
BAndreyG <wolf-irbis@yandex.ru>
2020-05-20 17:55:11 +1000

    vue rest

--- src/main/resources/static/js/v-main.js
+++ src/main/resources/static/js/v-main.js
@@ -1,6 +1,30 @@
+var messageApi=Vue.resource('/history/{id}');
+
+Vue.component('message-row',{
+    props:['message'],
+   template: '<div><i>({{ message.date }})</i> {{ message.client.name }}</div>'
+});
+Vue.component('messages-list', {
+    props:['mes'],
+    template: '<div><message-row v-for="message in mes"  :key="message.id"  :message="message" /></div>',
+    created:function(){
+        messageApi.get().then(result=>
+            result.json().then(
+                data=>data.forEach(message=>this.mes.push(message))
+            )
+        )
+    }
+});
+
 var app = new Vue({
     el: '#app',
+    template: '<messages-list :mes="messages" />',
     data: {
-        message: 'Привет, Vue!'
+        messages: [
+         /*   { id: '0', text: 'Овощи' },
+            { id: '1', text: 'Сыр' },
+            { id: '2', text: 'Что там ещё люди едят?' }*/
+        ]
+        // show:true
     }
-})
+});
